bd7d2e85b987285409ea827e81722e7bffe3b9e8
Chugunovaia <145828554+Chugunovaia@users.noreply.github.com>
2025-01-07 22:21:17 +0700

    not working

--- tcp-client.go
+++ tcp-client.go
@@ -2,25 +2,53 @@ package main
 
 import (
 	"bufio"
+	"crypto/ed25519"
 	"fmt"
 	"net"
 	"os"
 	"strings"
 	"time"
+
+	"crypto/curve25519"
 )
 
+func createKeys() (my_publicKey [32]byte, my_privateKey [32]byte) { //создаем личные ключи
+	pub, priv, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		fmt.Println("Error generating keys:", err)
+		return
+	}
+	copy(my_publicKey[:], pub[:])
+	copy(my_privateKey[:], priv[:])
+	curve25519.ScalarBaseMult(&my_publicKey, &my_privateKey) //домножаем, чтобы потом можно было получить общий ключ
+	fmt.Println("KEYS", err)
+	return
+}
+
+func createSharedKey(their_publicKey [32]byte, my_privateKey [32]byte) [32]byte {
+	var pubKey, privKey, secret [32]byte
+	copy(pubKey[:], their_publicKey[:])
+	copy(privKey[:], my_privateKey[:])
+	curve25519.ScalarMult(&secret, &privKey, &pubKey) //общий ключ, он будет одинаковым у двоих пользователей благодаря ScalarBaseMult
+	return secret
+}
+
 // прием данных из сокета и вывод на печать
-func readSock(conn net.Conn) {
+func readSock(conn net.Conn, my_privateKey [32]byte, codes map[[256]byte][32]byte) {
 
 	if conn == nil {
 		panic("Connection is nil")
 	}
 	buf := make([]byte, 256)
+
+	//message := make([]byte, 256)
+	//their_key := make([]byte, 32)
 	eof_count := 0
 	for {
 		// чистим буфер
 		for i := 0; i < 256; i++ {
 			buf[i] = 0
+
 		}
 
 		readed_len, err := conn.Read(buf)
@@ -44,23 +72,119 @@ func readSock(conn net.Conn) {
 			panic(err.Error())
 		}
 		if readed_len > 0 {
+			//		fmt.Println("MESSAGE")
+
+			/*	if (buf[0] == 227)||(buf[0]==64) {
+				var friend [256]byte
+				for i := 0; i < 256; i++ {
+					friend[i] = 0
+
+				}
+				var i = 0
+				for buf[i] != 64 {
+					i++
+				}
+				i++
+				var j = 0
+				for buf[i] != 13 && buf[i] != 32 && buf[i] != 33 && buf[i] != 63 && buf[i] != 46 && buf[i] != 44 {
+					friend[j] = buf[i]
+					j++
+					i++
+				}
+				_, ok := codes[friend]
+
+				if (buf[0]==227){
+					fmt.Println(227)
+				if ok {
+					delete(codes, friend)
+				}
+				} else {
+					fmt.Println(64)
+
+					if !ok {
+						i=0
+						//for buf[i]
+						//codes[friend]
+					} else {
+						//дешифровка
+					}
+				}
+
+
+			} */
 			fmt.Println(string(buf))
+
 		}
 
 	}
 }
 
-// ввод данных с консоли и вывод их в канал
-func readConsole(ch chan string) {
+func readConsole(ch chan string, codes map[[256]byte][32]byte, my_publicKey [32]byte) {
 	for {
+
 		line, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		if len(line) > 250 {
+
+		if len(line) > 150 {
 			fmt.Println("Error: message is very lagre")
 			continue
 		}
-		//fmt.Print(">")
-		out := line[:len(line)-1] // убираем символ возврата каретки
-		ch <- out                 // отправляем данные в канал
+		result := strings.Index(line, "@")
+		//	fmt.Println(result)
+
+		if result != -1 { //кому-то
+			var friend [256]byte
+			buf := make([]byte, 256)
+			for i := 0; i < 256; i++ {
+				buf[i] = 0
+				friend[i] = 0
+			}
+			var i = result + 1
+			var j = 0
+			for line[i] != 13 && line[i] != 32 && line[i] != 33 && line[i] != 63 && line[i] != 46 && line[i] != 44 {
+				friend[j] = line[i]
+				j++
+				i++
+			}
+			//	fmt.Println(friend)
+
+			_, ok := codes[friend]
+			if ok {
+				//шифрование
+			} else { //сообщение в виде @user(пробел=32)<сообщение(13)><публичный ключ>
+
+				buf[0] = 64
+				i := 0
+				for friend[i] != 0 {
+					buf[i+1] = friend[i]
+					i++
+				}
+				i++
+				buf[i] = 32
+				i++
+				//	fmt.Println(buf)
+				for j := 0; line[j] != 10; j++ {
+					//	fmt.Println(line[j])
+					buf[i] = line[j]
+					i++
+				}
+				//	fmt.Println(buf)
+				for j := 0; j < 32; j++ {
+					buf[i] = my_publicKey[j]
+					i++
+				}
+				//fmt.Println(buf)
+				line = string(buf)
+				//fmt.Println(my_publicKey)
+				//fmt.Println(buf)
+				//fmt.Println(line)
+			}
+
+		}
+		//a := "ㅁㅁ" //227
+
+		out := line //[:len(line)-1] // убираем символ возврата каретки
+
+		ch <- out // отправляем данные в канал
 	}
 }
 
@@ -74,10 +198,13 @@ func main() {
 		panic("Connection is nil")
 
 	}
-	fmt.Print("Firstly, enter your username, please: \nDo not use the ' ', '!', '?', '.', ','\n>")
+	fmt.Print("Firstly, enter your username in less than 8 ch, please: \nDo not use the ' ', '!', '?', '.', ',', '@'\n>")
+	my_conns := make(map[[256]byte][32]byte, 1024)
+	var my_privateKey, my_publicKey [32]byte
+	my_publicKey, my_privateKey = createKeys() //my_publicKey, my_privateKey)
 
-	go readConsole(ch)
-	go readSock(conn)
+	go readConsole(ch, my_conns, my_publicKey)
+	go readSock(conn, my_privateKey, my_conns)
 
 	for {
 		val, ok := <-ch
--- tcp-server.go
+++ tcp-server.go
@@ -9,18 +9,27 @@ import (
 func sentmessage(message string, username string) []byte {
 	builded := &strings.Builder{}
 	out_buf := make([]byte, 512)
+	plus_buf := make([]byte, 512)
 	//builded.WriteString(username)
-	builded.WriteString("->>")
+	builded.WriteString(" >>")
 	builded.WriteString(message)
 	builded.WriteString("\n")
 	//out_buf := make([]byte, 512)
 	extra_buf := []byte(builded.String())
 	out_buf = []byte(username)
 	j := 0
-	for i := 0; i < 256; i++ {
-		if out_buf[i] == 0 || out_buf[i] == 13 {
+	plus_buf[0] = 64
+	i := 1
+	for (out_buf[j] != 0) && (out_buf[j] != 13) {
+		plus_buf[i] = out_buf[j]
+		i++
+		j++
+	}
+	for ii := 0; i < 256; i++ {
+
+		if plus_buf[ii] == 0 || plus_buf[ii] == 13 {
 			if extra_buf[j] != 0 {
-				out_buf[i] = extra_buf[j]
+				plus_buf[ii] = extra_buf[j]
 				j++
 			} else {
 				break
@@ -28,7 +37,8 @@ func sentmessage(message string, username string) []byte {
 			}
 		}
 	}
-	return out_buf
+
+	return plus_buf
 }
 
 func nick(conn net.Conn, conns map[string]net.Conn, un_flag bool) {
@@ -68,51 +78,63 @@ func nick(conn net.Conn, conns map[string]net.Conn, un_flag bool) {
 
 			_, ok := conns[username]
 			if ok {
-				conn.Write([]byte("This username already exists. Please, enter different one:\n>"))
+				conn.Write([]byte("This username already exists. Please, enter different one:\n"))
 
-			} else if strings.Contains(username, " ") || strings.Contains(username, ",") || strings.Contains(username, ".") || strings.Contains(username, "!") || strings.Contains(username, "?") {
+			} else if strings.Contains(username, " ") || strings.Contains(username, ",") || strings.Contains(username, ".") || strings.Contains(username, "!") || strings.Contains(username, "?") || strings.Contains(username, "@") {
 				//fmt.Println(buf)
-				conn.Write([]byte("Used the forbidden symbol:\n>"))
+				conn.Write([]byte("Used the forbidden symbol:\n"))
 			} else {
 				conn.Write([]byte("Username accepted. \n"))
 				fmt.Println(username)
 				conns[username] = conn
-				conn.Write([]byte("To send a message to a specific user, enter their nickname as @username and the message. \nTo send a message to all users, simply leave the nickname out.\n>"))
+				conn.Write([]byte("To send a message to a specific user, enter their nickname as @username and the message. \nTo send a message to all users, simply leave the nickname out.\n "))
 
 				un_flag = true
 			}
 		} else {
-			result := strings.Index(message, "@")
-			if result == -1 {
+			//result := strings.Index(message, "@")
+			fmt.Println(buf[0])
+			if buf[0] != 64 {
 
 				//out_buf := make([]byte, 512)
 				for _, connection := range conns {
-					out_buf := sentmessage(message, username)
+					out_buf := []byte(message)
 					_, err2 := connection.Write(out_buf)
 					if err2 != nil {
 						fmt.Println("Error:", err2.Error())
+						_, ok := conns[username]
+						if ok {
+							delete(conns, username)
+						}
 						break
 					}
 
 				}
 			} else {
-
+				//	fmt.Println("HERE1")
 				var friend = ""
-				for i := result; i < 256; i++ {
+				for i := 0; i < 256; i++ {
+					//	fmt.Println(i)
 					if buf[i] == 64 {
 						i++
 						j := 0
+						//	fmt.Println(buf)
 						for buf[i] != 13 && buf[i] != 32 && buf[i] != 33 && buf[i] != 63 && buf[i] != 46 && buf[i] != 44 {
 							friend_buf[j] = buf[i]
 							j++
 							i++
 						}
+						//		fmt.Println(friend_buf)
 						friend_buf[j] = 13
 						break
 
 					}
+
 				}
+
 				friend = string(friend_buf)
+
+				fmt.Println(friend)
 
 				_, ok := conns[friend]
 
@@ -125,10 +147,14 @@ func nick(conn net.Conn, conns map[string]net.Conn, un_flag bool) {
 					_, err2 := conns[friend].Write(out_buf)
 					if err2 != nil {
 						fmt.Println("Error:", err2.Error())
+						_, ok := conns[username]
+						if ok {
+							delete(conns, username)
+						}
 						break
 					}
 				} else {
-					conn.Write([]byte("User does not exist or finished it's work. \n>"))
+					conn.Write([]byte("ㅁUser @" + friend + "does not exist or finished it's work. \n"))
 				}
 			}
 		}
@@ -140,8 +166,7 @@ func nick(conn net.Conn, conns map[string]net.Conn, un_flag bool) {
 func main() {
 
 	fmt.Println("Start server...")
-	// создаем пул соединений
-	//conns := make(map[int]net.Conn, 1024) //максимум 1024 соединеия
+
 	count_of_conns := 0
 	my_conns := make(map[string]net.Conn, 1024)
 	// Устанавливаем прослушивание порта
@@ -151,14 +176,9 @@ func main() {
 		// Принимаем входящее соединение
 		if count_of_conns < 1024 {
 			conn, _ := ln.Accept()
-			// сохраняем соединение в пул
-			//conns[count_of_conns] = conn
-			// запускаем функцию process(conn)   как горутину
 			go nick(conn, my_conns, false)
-			//count_of_conns--
 			count_of_conns = len(my_conns)
-			//fmt.Println("Number of connections:", count_of_conns)
-			//	go process(conns, count_of_conns)
+
 		}
 
 	}
